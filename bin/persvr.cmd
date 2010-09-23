1717bc7a47d4632b3b4e4fc8fd1a6106e788997f
Dustin Machi <dmachi@dojotoolkit.org>
2010-09-23 18:37:34 -0400

    update the persvr commands

--- bin/persvr.cmd
+++ bin/persvr.cmd
@@ -5,10 +5,12 @@ setlocal
 set SHELL=cmd.exe
 
 set PACKAGE_HOME=%~dp0..
+set NODULES_PATH=%~dp0..
 
 call %PACKAGE_HOME%\packages\narwhal\bin\activate.cmd
 
 set NARWHAL_PATH=%PACKAGE_HOME%
+set NODULES_PATH=%PACKAGE_HOME%
 set PERSVR_APP=$1
 cd %1
 set SEA=%cd%
@@ -25,4 +27,4 @@ set params=%params% %1
 goto loop
 :afterloop
 
-%PACKAGE_HOME%\packages\jack\bin\jackup %params%
+%PACKAGE_HOME%\packages\jack\bin\jackup
